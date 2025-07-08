e3b0a39c79b5759a1bb29118ebd21198d13a35d9
AbdelrhmanAFayed <abdelrhmanfayed2002@gmail.com>
2025-07-08 18:56:50 +0300

    Added Can setup

--- can_setup/can_setup.sh
+++ can_setup/can_setup.sh
@@ -5,3 +5,4 @@ export PATH=/system/bin:/vendor/bin:$PATH
 ip link set can0 down
 ip link set can0 type can bitrate 500000
 ip link set can0 up
+cmd overlay enable --user 0  com.android.systemui.rro.bottom.rounded
